c27b023faa0d170cd7a993d56326b9035a4d0002
Malak <malaksanchez09@gmail.com>
2026-01-18 19:31:20 -0500

    App Script Update

--- portfolio/app.js
+++ portfolio/app.js
@@ -2,53 +2,44 @@ const projectsData = [
     {
         id: '01',
         title: 'Color Palette',
-        description: "Frontend utility for generating dynamic color palettes.",
+        description: 'Frontend utility for generating dynamic color palettes.',
         tags: ['HTML', 'CSS', 'JS'],
-        link: "../projects/color_palette/index.html",
-        image: "assets/color_palette.png"
+        link: 'projects/color_palette/index.html',
+        image: 'assets/color_palette.png'
     },
     {
         id: '02',
         title: 'Password Generator',
         description: 'Client secure password generator.',
         tags: ['HTML', 'CSS', 'JS'],
-        link: "../projects/password_generator/index.html",
-        image: "assets/password_generator.png"
+        link: 'projects/password_generator/index.html',
+        image: 'assets/password_generator.png'
     },
     {
         id: '03',
-        title: 'Quiz Game',
-        description: 'Fullstack quiz platform with backend logic.',
-        tags: ['Node', 'Express', 'JS', 'Gemini API'],
-        link: "../projects/quiz_game/frontend/index.html",
-        image: "assets/quiz_game.png"
-    },
-    {
-        id: '04',
         title: 'Pricing Card',
         description: 'Pricing card frontend project.',
         tags: ['CSS', 'HTML'],
-        link: "../projects/pricing_cards/index.html",
-        image: "assets/pricing_card.png"
+        link: 'projects/pricing_cards/index.html',
+        image: 'assets/pricing_card.png'
     },
     {
-        id: '05',
+        id: '04',
         title: 'Github Profile Finder',
         description: 'Github profile finder using Github API.',
         tags: ['CSS', 'HTML', 'JS', 'Github'],
-        link: "../projects/github_finder/index.html",
-        image: "assets/github_finder.png"
+        link: 'projects/github_finder/index.html',
+        image: 'assets/github_finder.png'
     },
     {
-        id: '06',
+        id: '05',
         title: 'Tempus Mortis',
         description: 'A terror digital clock.',
         tags: ['CSS', 'HTML', 'JS'],
-        link: "../projects/tempus_mortis/index.html",
-        image: "assets/tempus_mortis.png"
+        link: 'projects/tempus_mortis/index.html',
+        image: 'assets/tempus_mortis.png'
     }
 ];
-
 class PortfolioRenderer {
     constructor(data) {
         this.projects = data;
